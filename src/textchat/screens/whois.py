b7ff41d87fced6b7fa9f3e5d7dc389c3b1be2f3d
rmblau <mrb1105@gmail.com>
2024-09-08 21:12:23 -0400

    update version, add bug fix for PMs, add pre-commit

--- src/textchat/screens/whois.py
+++ src/textchat/screens/whois.py
@@ -1,12 +1,16 @@
 from textual.app import ComposeResult
 from textual.containers import Grid
 from textual.screen import ModalScreen
-from textual.widgets import Button, Label, TabbedContent, TabPane
+from textual.widgets import Button
+from textual.widgets import Label
+from textual.widgets import TabbedContent
+from textual.widgets import TabPane
+
 
 class WhoisScreen(ModalScreen[bool]):
     def compose(self) -> ComposeResult:
         yield Grid(
-            Label(f"Sent PM to this user?", id="question"),
+            Label("Sent PM to this user?", id="question"),
             Button("Private Message", variant="error", id="message"),
             Button("Cancel", variant="primary", id="cancel"),
             id="dialog",
@@ -15,15 +19,26 @@ class WhoisScreen(ModalScreen[bool]):
     def on_button_pressed(self, event: Button.Pressed) -> None:
         if event.button.id == "message":
             try:
-                self.tab = self.app.irc_screen().query_one(TabbedContent).get_pane(f'{self.app.user.replace("@", "").replace("+", "")}')
-                    
+                self.tab = (
+                    self.app.irc_screen()
+                    .query_one(TabbedContent)
+                    .get_pane(f'{self.app.user.replace("@", "").replace("+", "")}')
+                )
+
             except:
-                self.active_tab = self.app.SCREENS['irc'].query_one(TabbedContent).add_pane(TabPane(self.app.user.replace("@", "").replace("+", ""), 
-                                                                              Label(),
-                                                                              name=self.app.user.replace("@", "").replace("+", ""), 
-                                                                              id=f'{self.app.user.replace("@", "").replace("+", "")}'))
+                self.active_tab = self.app.SCREENS["irc"].query_one(TabbedContent)
+                self.active_tab.add_pane(
+                    TabPane(
+                        self.app.user.replace("@", "").replace("+", ""),
+                        Label(),
+                        name=self.app.user.replace("@", "").replace("+", ""),
+                        id=f'{self.app.user.replace("@", "").replace("+", "")}',
+                    )
+                )
+                self.active_tab.active = self.app.user.replace("@", "").replace("+", "")
+
             self.dismiss(True)
-            
+
         else:
 
             self.dismiss(False)
